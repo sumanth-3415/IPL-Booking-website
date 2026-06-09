c007fb928fcb5bf61a51d47b895714bcd198dccc
sumanth-3415 <lingarisumanthkumarreddy@gmail.com>
2026-06-09 23:19:17 +0530

    Updated IPL Ticket Booking Project

--- IPL Booking/Backend/server.js
+++ IPL Booking/Backend/server.js
@@ -41,5 +41,5 @@ app.use("/api/bookings", bookingRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
